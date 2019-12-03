b1646d41f3fc6ab09b16d8874edf189c98e42a33
Sam Hart <sam.hart.swanson@gmail.com>
2019-12-02 22:11:40 -0500

    Completed day 2 puzzle

--- src/__tests__/day-2/index.ts
+++ src/__tests__/day-2/index.ts
@@ -1,5 +1,28 @@
-import { add } from '@app/day-2'
+import { part1, part2, computeState } from '@app/day-2'
+import testInput from '@test/day-2/testInput.txt'
+import challengeInput from '@test/day-2/challengeInput.txt'
 
-test('true', () => {
-  expect(add(1, 1)).toBe(2)
+test('Compute state should add from and store in correct locations', () => {
+  expect(computeState([1, 0, 0, 0, 99])).toEqual([2, 0, 0, 0, 99])
+})
+
+test('computeState should multiply from and store in correct locations', () => {
+  expect(computeState([2, 3, 0, 3, 99])).toEqual([2, 3, 0, 6, 99])
+})
+
+test('computeState should handle multiple operations', () => {
+  expect(computeState([1, 1, 1, 4, 99, 5, 6, 0, 99]))
+    .toEqual([30, 1, 1, 4, 2, 5, 6, 0, 99])
+})
+
+test('The calculated value from the test input for part 1 should be 3500', () => {
+  expect(computeState(testInput.trim().split(',').map(Number))[0]).toBe(3500)
+})
+
+test('The calculated value from the challenge input for part 1 should be 3562672', () => {
+  expect(part1(challengeInput)).toBe(3562672)
+})
+
+test('The calculated value from the challenge input for part 2 should be 8250', () => {
+  expect(part2(challengeInput, 19690720)).toBe(8250)
 })
--- src/day-2/index.ts
+++ src/day-2/index.ts
@@ -1,3 +1,56 @@
-export function add (a: number, b: number): number {
-  return a + b
+export function part1 (puzzleInput: string): number {
+  const intCodes = puzzleInput
+    .trim()
+    .split(',')
+    .map(Number)
+  intCodes[1] = 12
+  intCodes[2] = 2
+  const finalState = computeState([...intCodes])
+
+  return finalState[0]
+}
+
+export function part2 (puzzleInput: string, targetValue: number): number {
+  const intCodes = puzzleInput
+    .trim()
+    .split(',')
+    .map(Number)
+
+  for (let i = 0; i < 100; i++) {
+    for (let j = 0; j < 100; j++) {
+      try {
+        const modifiedIntCodes = [...intCodes]
+        modifiedIntCodes[1] = i
+        modifiedIntCodes[2] = j
+        const computedState = computeState(modifiedIntCodes)
+        if (computedState[0] === targetValue) {
+          return 100 * i + j
+        }
+      } catch (e) {
+        console.error(e)
+        continue
+      }
+    }
+  }
+  return 0
+}
+
+export function computeState (intCodes: Array<number>): Array<number> {
+  for (let i = 0; i < intCodes.length; i += 4) {
+    const operation = intCodes[i]
+    if (operation === 99) {
+      return intCodes
+    }
+    const leftOperandLocation = intCodes[i + 1]
+    const rightOperandLocation = intCodes[i + 2]
+    const resultStorageLocation = intCodes[i + 3]
+    if (operation === 1) {
+      intCodes[resultStorageLocation] =
+        intCodes[leftOperandLocation] + intCodes[rightOperandLocation]
+    } else if (operation === 2) {
+      intCodes[resultStorageLocation] =
+        intCodes[leftOperandLocation] * intCodes[rightOperandLocation]
+    }
+  }
+  return intCodes
 }
