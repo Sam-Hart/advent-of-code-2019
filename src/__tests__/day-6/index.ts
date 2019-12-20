b633075743164733f45d80a84e17aa9ef6ac0f98
Sam Hart <sam.hart.swanson@gmail.com>
2019-12-20 16:56:57 -0500

    Part 2 completed

--- src/__tests__/day-6/index.ts
+++ src/__tests__/day-6/index.ts
@@ -1,4 +1,4 @@
-import { part1, part2 } from '@app/day-6'
+import { part1, part2, Node } from '@app/day-6'
 import testInput from '@test/day-6/testInput.txt'
 import sanInput from '@test/day-6/sanInput.txt'
 import challengeInput from '@test/day-6/challengeInput.txt'
@@ -11,6 +11,59 @@ test('Challenge input should have 270768 orbits', () => {
   expect(part1(challengeInput)).toEqual(270768)
 })
 
+test('Distance between a parent and child node should be 1', () => {
+  const a = new Node('A')
+  const b = new Node('B')
+  a.addChild(b)
+  expect(a.distanceFrom(b)).toBe(1)
+})
+
+test('Distance between a child node and parent should be 1', () => {
+  const a = new Node('A')
+  const b = new Node('B')
+  a.addChild(b)
+  expect(b.distanceFrom(a)).toEqual(1)
+})
+
+test('Distance from a parent node to an indirect child node with 2 intermediary nodes should be 3', () => {
+  const a = new Node('A')
+  const b = new Node('B')
+  const c = new Node('C')
+  const d = new Node('D')
+  a.addChild(b)
+  b.addChild(c)
+  c.addChild(d)
+  expect(a.distanceFrom(d)).toEqual(3)
+  expect(d.distanceFrom(a)).toEqual(3)
+  expect(d.distanceFrom(c)).toEqual(1)
+})
+
+test('Distance between two disparate nodes with a common ancestor should be calculated correctly', () => {
+  const a = new Node('A')
+  const b = new Node('B')
+  const c = new Node('C')
+  const d = new Node('D')
+  const e = new Node('E')
+  const f = new Node('F')
+  const g = new Node('G')
+  const h = new Node('H')
+  const i = new Node('I')
+  a.addChild(b)
+  b.addChild(c)
+  c.addChild(d)
+  b.addChild(e)
+  c.addChild(f)
+  f.addChild(g)
+  c.addChild(h)
+  h.addChild(i)
+  expect(g.distanceFrom(e)).toEqual(4)
+  expect(g.distanceFrom(i)).toEqual(4)
+})
+
 test('Distance from Santa with test input should be 4', () => {
   expect(part2(sanInput)).toEqual(4)
+})
+
+test('Distance from santa with challenge input is', () => {
+  expect(part2(challengeInput)).toEqual(451)
 })
--- src/day-6/index.ts
+++ src/day-6/index.ts
@@ -1,3 +1,6 @@
+import * as util from 'util'
+import { Maybe } from 'tsmonad'
+
 export function part1 (puzzleInput: string): number {
   const orbits = puzzleInput
     .trim()
@@ -21,8 +24,10 @@ export function part2 (puzzleInput: string): number {
     .map(orbitDescription => orbitDescription.split(')'))
 
   const orbitList: Array<Node> = generateOrbitList(orbits)
-  console.log(orbitList)
-  return 0
+  const santa = orbitList.find(node => node.name === 'SAN')!
+  const you = orbitList.find(node => node.name === 'YOU')!
+
+  return you.parent.distanceFrom(santa.parent)
 }
 
 function generateOrbitList (orbits: Array<Array<string>>): Array<Node> {
@@ -52,7 +57,7 @@ function calculateOrbits (orbit: Node): number {
 }
 
 
-class Node {
+export class Node {
   name: string
   _parent: Node
   _children: Array<Node> = []
@@ -64,11 +69,60 @@ class Node {
 
   addChild (child: Node) {
     this._children.push(child)
+    child.parent = this
+  }
+
+  distanceFrom (target: Node): number {
+    let distance = 0
+    if (target === this) {
+      return distance
+    }
+    const possibleChild = this.children.find(child => target === child)
+    if (possibleChild) {
+      distance += 1
+      return distance
+    }
+    if (this.indirectChildren.includes(target)) {
+      for (let i = 0; i < this.children.length; i++) {
+        const child = this.children[i]
+        if (child.children.includes(target) || child.indirectChildren.includes(target)) {
+          distance += this.children[i].distanceFrom(target) + 1
+          return distance
+        }
+      }
+    }
+
+    if (
+      target !== this &&
+      !this.children.includes(target) &&
+      !this.indirectChildren.includes(target) &&
+      this.parent
+    ) {
+      distance += this.parent.distanceFrom(target) + 1
+    }
+
+    return distance
   }
 
   set parent (parent: Node) {
     this._parent = parent
-    this._parent.addChild(this)
+    if (!this._parent.children.includes(this)) {
+      this._parent.addChild(this)
+    }
+  }
+
+  get tree (): Array<Node> {
+    const tree = this.children.reduce((children, child) => {
+      return children.concat(child.tree)
+    }, new Array<Node>())
+    tree.push(this)
+    return tree
+  }
+
+  get indirectChildren () {
+    return this.children.reduce((indirectChildren, child) => {
+      return indirectChildren.concat(child.tree)
+    }, new Array<Node>())
   }
 
   get children () {
